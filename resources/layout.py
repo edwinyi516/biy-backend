b43c82bd26d07b270b8903447b427e651ce6208c
Edwin <edwin.yi.128@gmail.com>
2022-11-24 14:25:47 -0600

    New user basic layout data changed to receive from frontend, added layout update route

--- resources/layout.py
+++ resources/layout.py
@@ -10,13 +10,24 @@ layout = Blueprint('layouts', 'layout')
 @layout.route('/new', methods = ['POST'])
 def new_layout():
     payload = request.get_json()
-    new_layout = models.Layout.create(user = current_user.id, layout_data = payload('layout'))
+    new_layout = models.Layout.create(**payload)
     layout_dict = model_to_dict(new_layout)
     return jsonify(
         data = layout_dict,
         message = 'New layout created',
         status = 201
     ), 201
+
+@layout.route('/update', methods = ['PUT'])
+def update_layout():
+    payload = request.get_json()
+    query = models.Dog.update(**payload).where(models.Layout.user == current_user.id)
+    query.execute()
+    return jsonify(
+        data = model_to_dict(models.Layout.get(models.Layout.user) == current_user.id),
+        status = 200,
+        message = 'Layout successfully updated'
+    ), 200
 
 @layout.route('/')
 def get_user_layout():
